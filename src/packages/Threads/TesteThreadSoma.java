dc168dd83a8ce2614c174cfdfd9b62bd5e1420c0
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-26 09:53:39 -0300

    Sincornizando métodos em threads

---
+++ src/packages/Threads/TesteThreadSoma.java
@@ -0,0 +1,9 @@
+package packages.Threads;
+
+public class TesteThreadSoma {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+		MinhaThreadSoma t1 = new MinhaThreadSoma("#1", array);
+		MinhaThreadSoma t2 = new MinhaThreadSoma("#2", array);
+    }
+}
