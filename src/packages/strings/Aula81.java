04ca1ddad5b9eb0e84e994a8e191cff9d72db845
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-26 11:25:44 -0300

    Métodos toLowerCase() e toUpperCase()

---
+++ src/packages/strings/Aula81.java
@@ -0,0 +1,15 @@
+package packages.strings;
+
+public class Aula81{
+    public static void main(String[] args) {
+        String teste = "Teste";
+		
+		String testeMinusc = teste.toLowerCase();
+		String testeMaisc = teste.toUpperCase();
+
+		System.out.println(testeMinusc);
+		System.out.println(testeMaisc);
+		
+		//if (teste.toLowerCase().equals(teste.toLowerCase()))
+    }
+}
