17424951efd60999b0d601af77db7a297a29cb4c
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-26 11:46:35 -0300

    Classe StringTokenizer, utilitaria para extrair informações de um string com separadores, mais comum utiliza na leitura de arquivos

---
+++ src/packages/strings/Aula84.java
@@ -0,0 +1,15 @@
+package packages.strings;
+
+import java.util.StringTokenizer;
+
+public class Aula84 {
+    public static void main(String[] args) {
+        String doArquivo = "1;Antônio;30;";
+		
+		StringTokenizer st = new StringTokenizer(doArquivo, ";");
+		
+		while (st.hasMoreTokens()){
+			System.out.println(st.nextToken());
+		}
+    }
+}
